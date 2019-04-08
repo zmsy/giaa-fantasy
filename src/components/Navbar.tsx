4073f22b7d0b203f42fd298c57f0e4d3c179e57a
Zach Morrissey <zachmor05@gmail.com>
2019-04-07 21:53:11 -0700

    switch to scss and add in variables for color scheme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import './Navbar.css'
 class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar navbar-light">
         <a className="navbar-brand" href="#">
           <img src={giaaLogo} className="Navbar-logo" alt="" />
           GIAA Fantasy Baseball
