1b58484d092c7dba867c4b1065371066d2d923fa
Zach Morrissey <zachmor05@gmail.com>
2019-04-07 19:38:10 -0700

    import bootstrap and create nav component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Nav from './Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p> This is the start of GIAA Fantasy. </p>
---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Nav extends React.Component {
+  render() {
+    return "This is my navbar!";
+  }
+}
+
+export default Nav;
