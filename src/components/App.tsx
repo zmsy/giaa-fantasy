b314229107383167c59cbfa550df7da03fb3d185
Zach Morrissey <zachmor05@gmail.com>
2019-04-08 21:33:18 -0700

    add logos and add entries to topnav

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import logo from '../branding/logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
-import giaaLogo from '../giaa-logo.svg';
-import './Navbar.css'
+import giaaLogo from '../branding/giaa-logo.svg';
+import './Navbar.scss'
 
 class Navbar extends React.Component {
   render() {
+
+    // let navElements = [
+    //   ["Players", "/players"],
+    //   ["Squads", "/squads"],
+    //   ["Matchups", "/matchups"]
+    // ];
+
+    // for (let entry of navElements) {
+    //   console.log(entry);
+    // }
+
     return (
       <nav className="navbar navbar-light">
         <a className="navbar-brand" href="#">
           <img src={giaaLogo} className="Navbar-logo" alt="" />
-          GIAA Fantasy Baseball
+          <span className="Navbar-brand-text">GIAA <span className="Navbar-red">Fantasy Baseball</span></span>
         </a>
+          <ul className="navbar-nav ml-auto Navbar-right">
+            <li className="nav-item Navbar-item-right">
+              <a className="nav-link">Players</a>
+            </li>
+            <li className="nav-item Navbar-item-right">
+              <a className="nav-link">Squads</a>
+            </li>
+            <li className="nav-item Navbar-item-right">
+              <a className="nav-link">Matchups</a>
+            </li>
+          </ul>
       </nav>
     );
   }
