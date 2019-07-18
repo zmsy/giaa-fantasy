8bbfd7928d071a8aeb601deaa65547b352df449e
Zach Morrissey <zachmor05@gmail.com>
2019-07-17 19:51:07 -0700

    fix landingpagestate/props

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,32 +1,45 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
-import { Player, PlayerProps }  from './Player';
+import { /*Player,*/ PlayerProps }  from './Player';
 
-// /Users/zach/x/giaa-fantasy/src/components/LandingPage/Player.tsx
+interface LandingPageProps {}
 
-type LandingPageProps = {
-  players: number
-  playerList: PlayerProps[]
+interface LandingPageState {
+  players: number,
+  playerList: Array<PlayerProps>
 }
 
-class LandingPage extends React.Component<LandingPageProps> {
+class LandingPage extends React.Component<LandingPageProps, LandingPageState> {
 
-  static defaultProps = {
-    players: 0,
-    playerList: []
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      players: 0,
+      playerList: new Array<PlayerProps>()
+    }
   }
 
   componentDidMount() {
     console.log("Component mounted successfully!");
+    this.getPlayers(12);
+  }
+
+  async getPlayers(playerId: number) {
+    let response = await fetch("http://localhost:8000/api/players/" + playerId)
+    let data = await response.json();
+    let player = data.data as PlayerProps;
+    this.state.playerList.push(player);
+    console.log(data);
+    return data;
   }
 
   render() {
     return (
       <Jumbotron fluid>
         <h1>Hello world!</h1>
-        this.props.playerList.forEach(element => {
+        {this.state.playerList.forEach(element => {
           // <Player />
-        });
+        })}
       </Jumbotron>
     );
   }
